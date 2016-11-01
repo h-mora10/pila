8cbe949ef0450cd92d797afda4958ad7842a24f9
juanshbta69 <juan.shbta69@gmail.com>
2016-10-31 21:59:36 -0500

    Manejo de errores en los servicios de creación y actualización de aportantes e implementacuión del servicio para eliminar un aportante

--- PilaAutomatitation/pila/views.py
+++ PilaAutomatitation/pila/views.py
@@ -1,8 +1,10 @@
+# coding=utf-8
 import json
 
 from django.contrib.auth.models import User
 from django.core import serializers
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
@@ -14,45 +16,62 @@ from .models import OperadorServicio, Aportante
 
 @csrf_exempt
 def crear_aportante(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
+    try:
+        if request.method == 'POST':
+            data = json.loads(request.body)
 
-        nombre_usuario = data['usuario']
-        contrasenia = data['password']
-        nombre = data['nombre']
-        tipo_pagador_pensiones = data['tipoPagador']
+            nombre_usuario = data['usuario']
+            contrasenia = data['password']
+            nombre = data['nombre']
+            tipo_pagador_pensiones = data['tipoPagador']
 
-        usuario = User.objects.create_user(username=nombre_usuario, password=contrasenia)
-        operador_servicio = OperadorServicio.objects.get(pk=1)
+            usuario = User.objects.create_user(username=nombre_usuario, password=contrasenia)
+            operador_servicio = OperadorServicio.objects.get(pk=1)
 
-        aportante = Aportante()
-        aportante.usuario_id = usuario
-        aportante.nombre = nombre
-        aportante.tipo_pagador_pensiones = tipo_pagador_pensiones
-        aportante.operador_servicio = operador_servicio
-        aportante.save()
+            aportante = Aportante()
+            aportante.usuario_id = usuario
+            aportante.nombre = nombre
+            aportante.tipo_pagador_pensiones = tipo_pagador_pensiones
+            aportante.operador_servicio = operador_servicio
+            aportante.save()
 
-        return HttpResponse(serializers.serialize("json", [aportante]))
+            return HttpResponse(serializers.serialize("json", [aportante]))
+    except:
+        return JsonResponse({"mensaje": "Ocurrió un error creando el aportante"})
 
 
 @csrf_exempt
 def actualizar_aportante(request, id):
-    if request.method == 'PUT':
-        data = json.loads(request.body)
+    try:
+        if request.method == 'PUT':
+            data = json.loads(request.body)
 
-        aportante = Aportante.objects.get(pk=id)
-        usuario = User.objects.get(pk=aportante.usuario_id.id)
+            aportante = Aportante.objects.get(pk=id)
+            usuario = User.objects.get(pk=aportante.usuario_id.id)
 
-        nombre_usuario = data['usuario']
-        contrasenia = data['password']
-        nombre = data['nombre']
-        tipo_pagador_pensiones = data['tipoPagador']
+            nombre_usuario = data['usuario']
+            contrasenia = data['password']
+            nombre = data['nombre']
+            tipo_pagador_pensiones = data['tipoPagador']
 
-        usuario.password = contrasenia
-        usuario.save()
+            usuario.password = contrasenia
+            usuario.save()
 
-        aportante.nombre = nombre
-        aportante.tipo_pagador_pensiones = tipo_pagador_pensiones
-        aportante.save()
+            aportante.nombre = nombre
+            aportante.tipo_pagador_pensiones = tipo_pagador_pensiones
+            aportante.save()
 
-        return HttpResponse(serializers.serialize("json", [aportante]))
+            return HttpResponse(serializers.serialize("json", [aportante]))
+    except:
+        return JsonResponse({"mensaje": "Ocurrió un error actualizando el aportante " + str(id)})
+
+
+@csrf_exempt
+def eliminar_aportante(request, id):
+    try:
+        if request.method == 'DELETE':
+            aportante = Aportante.objects.get(pk=id)
+            aportante.delete()
+            return JsonResponse({"mensaje": "ok"})
+    except:
+        return JsonResponse({"mensaje": "Ocurrió un error eliminando el aportante " + str(id)})
