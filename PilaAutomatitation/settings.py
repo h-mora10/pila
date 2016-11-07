76a71906579ec2425d894a901891f9db941b6914
Henry Mora <hank8617@gmail.com>
2016-11-06 23:22:29 -0500

    Configuración de stafic files

--- PilaAutomatitation/settings.py
+++ PilaAutomatitation/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,3 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+        #This lets Django's collectstatic store our bundles
+        os.path.join(BASE_DIR, 'docs/assets'),
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
